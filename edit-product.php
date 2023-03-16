70cfbd51b638be9e02496fd28b5872f6eec09b7b
Daudmir <74597831+Daudmir@users.noreply.github.com>
2023-03-16 18:09:44 +0100

    Create edit-product.php

---
+++ edit-product.php
@@ -0,0 +1,11 @@
+<?php 
+$database = 'webshop_db';
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+
+$connection = new mysqli($servername, $username, $password, $database);
+
+
+
+?>
